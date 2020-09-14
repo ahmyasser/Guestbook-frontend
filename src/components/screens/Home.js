5223747d4cd84cf1a4247bb69894adc9c8e262e7
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-14 11:10:10 +0200

    added home page UI

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Home = ()=>{
     return(
-<h1>Home</h1>
-    );
+<div className="home">
+<div className="card home-card">
+<h5>name</h5>
+<div className="message">
+<h4>title</h4>
+<p>hello this is the body please take a look and let me know if you like it....</p>
+<div className="row">
+<input class="col s10" type="text"placeholder="send a reply...."/>
+<i class="material-icons col s2">send</i>
+</div>
+</div>
+</div>
+</div>    
+);
 }
 
-export default Home;
+export default Home
