f027d5b280a1c3accd0055ed378facdc94a60b3d
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-14 06:31:51 +0200

    added signup UI

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -5,7 +5,7 @@ const Login = ()=>{
     return(
         <div className="mycard">
         <div className="card auth-card input-field">
-          <h2>Instagram</h2>
+          <h2>Guestbook</h2>
           <input
           type="text"
           placeholder="email"
--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,8 +1,42 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 const Signup = ()=>{
-    return(
-<h1>Signup</h1>
+    return( <div className="mycard">
+    <div className="card auth-card input-field">
+      <h2>Guestbook</h2>
+      <input
+      type="text"
+      placeholder="name"
+      //value={name}
+      //onChange={(e)=>setName(e.target.value)}
+      />
+      <input
+      type="text"
+      placeholder="email"
+      //value={email}
+      //onChange={(e)=>setEmail(e.target.value)}
+      />
+      <input
+      type="password"
+      placeholder="password"
+      //value={password}
+      //onChange={(e)=>setPasword(e.target.value)}
+      />
+      <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+      //onClick={()=>PostData()}
+      >
+          SignUP
+      </button>
+      <h5>
+          <Link to="/login">Login</Link>
+      </h5>
+       
+         
+   
+      
+
+  </div>
+</div>
     );
 }
 
