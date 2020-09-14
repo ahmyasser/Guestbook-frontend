86fc0a58addaf54701548f1bee0d62434c0f742c
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-14 06:03:00 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/screens/Home';
+import Profile from './components/screens/Profile';
+import Login from './components/screens/Login';
+import Signup from './components/screens/Signup';
 import './App.css';
-
+import {BrowserRouter, Route} from 'react-router-dom'
 function App() {
-  return(  
-    <Navbar/>    
+  return(
+    <BrowserRouter>  
+    <Navbar />
+    <Route exact path="/">
+    <Home />
+    </Route>
+    <Route path="/profile">
+    <Profile />
+    </Route>
+    <Route path="/login">
+    <Login />
+    </Route>   
+    <Route path="/signup">
+    <Signup />
+    </Route>   
+    </BrowserRouter> 
 )}
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Navbar = ()=>{
 
     return(
       <nav>
       <div className="nav-wrapper grey lighten-5">
-        <a href="/" className="brand-logo left">Guestbook</a>
+        <Link to="/" className="brand-logo left">Guestbook</Link>
         <ul id="nav-mobile" className="right ">
-          <li><a href="sass.html">Login</a></li>
-          <li><a href="badges.html">Signup</a></li>
-          <li><a href="collapsible.html">Myposts</a></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Signup</Link></li>
+          <li><Link to="/profile">Myposts</Link></li>
         </ul>
       </div>
     </nav>
