11a89c3f35a02129f6063137ffbbef0c8ebf202c
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-14 06:20:46 +0200

    added login form

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React  from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = ()=>{
     return(
-<h1>Login</h1>
+        <div className="mycard">
+        <div className="card auth-card input-field">
+          <h2>Instagram</h2>
+          <input
+          type="text"
+          placeholder="email"
+          //value={email}
+          //onChange={(e)=>setEmail(e.target.value)}
+          />
+          <input
+          type="password"
+          placeholder="password"
+          //value={password}
+          //onChange={(e)=>setPasword(e.target.value)}
+          />
+          <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          //onClick={()=>PostData()}
+          >
+              Login
+          </button>
+          <h5>
+              <Link to="/signup">Signup</Link>
+          </h5>
+          <h6>
+              <Link to="/reset">Forgot password</Link>
+          </h6>
+  
+      </div>
+    </div>
     );
 }
 
