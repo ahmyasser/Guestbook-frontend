9ca92728a8794c5d834427cc4debc1968b015257
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-15 06:59:28 +0200

    get messages now dynamic

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,19 +1,33 @@
-import React from 'react';
-
+import React,{useState, useEffect} from 'react';
 const Home = ()=>{
+    const [data,setData] = useState([])
+    useEffect(()=>{
+       fetch('/posts',{
+           headers:{
+               "Authorization":"Bearer "+localStorage.getItem("jwt")
+           }
+       }).then(res=>res.json())
+       .then(result=>{
+           setData(result.posts)
+       })
+    },[]);
+
     return(
 <div className="home">
-<div className="card home-card">
-<h5>name</h5>
+{
+    data.map(item=>{
+        return(
+<div className="card home-card" key={item._id}>
+<h5>{item.postedBy.name}</h5>
 <div className="message">
-<h4>title</h4>
-<p>hello this is the body please take a look and let me know if you like it....</p>
+<h4>{item.title}</h4>
+<p>{item.body}</p>
 <div className="row">
 <input className="col s10" type="text"placeholder="send a reply...."/>
 <i className="material-icons col s2">send</i>
 </div>
 </div>
-</div>
+</div>)})}
 </div>    
 );
 }
